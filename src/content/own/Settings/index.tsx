76278a4967e20265b1cd055cf31b03328a493348
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2022-09-04 00:19:46 +0100

    chore: init general settings

--- src/content/own/Settings/index.tsx
+++ src/content/own/Settings/index.tsx
@@ -1,17 +1,21 @@
-import { useState } from 'react';
-import { Avatar, Box, Button, Divider, Grid, styled, Typography, useTheme } from '@mui/material';
-
-import TeamOverview from './TeamOverview';
-import TasksAnalytics from './TasksAnalytics';
-import Performance from './Performance';
-import Projects from './Projects';
-import Checklist from './Checklist';
-import Profile from './Profile';
-import TaskSearch from './TaskSearch';
+import {
+  Avatar,
+  Box,
+  Divider,
+  Grid,
+  MenuItem,
+  Select,
+  styled,
+  Switch,
+  Typography,
+  useTheme
+} from '@mui/material';
 import { useTranslation } from 'react-i18next';
-import NotificationsActiveTwoToneIcon from '@mui/icons-material/NotificationsActiveTwoTone';
 import SettingsLayout from './SettingsLayout';
-
+import { Formik, Field } from 'formik';
+import * as Yup from 'yup';
+import wait from '../../../utils/wait';
+import { Label } from '@mui/icons-material';
 
 const AvatarPrimary = styled(Avatar)(
   ({ theme }) => `
@@ -30,117 +34,153 @@ const AvatarPrimary = styled(Avatar)(
 function DashboardTasks() {
   const { t }: { t: any } = useTranslation();
   const theme = useTheme();
-
-  const [currentTab, setCurrentTab] = useState<string>('analytics');
+  const onSubmit = async (
+    _values,
+    { resetForm, setErrors, setStatus, setSubmitting }
+  ) => {
+    try {
+      await wait(1000);
+      resetForm();
+      setStatus({ success: true });
+      setSubmitting(false);
+    } catch (err) {
+      console.error(err);
+      setStatus({ success: false });
+      setErrors({ submit: err.message });
+      setSubmitting(false);
+    }
+  };
   return (
     <SettingsLayout>
-      {currentTab === 'analytics' && (
-        <>
-          <Grid item xs={12}>
-            <Box p={4}>
-              <TeamOverview />
-            </Box>
-          </Grid>
-          <Grid item xs={12}>
-            <Divider />
-            <Box
-              p={4}
-              sx={{
-                background: `${theme.colors.alpha.black[5]}`
-              }}
-            >
-              <Grid container spacing={4}>
-                <Grid item xs={12} sm={6} md={8}>
-                  <TasksAnalytics />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                  <Performance />
-                </Grid>
-              </Grid>
-            </Box>
-            <Divider />
-          </Grid>
-          <Grid item xs={12}>
-            <Box p={4}>
-              <Projects />
-            </Box>
-            <Divider />
-          </Grid>
-          <Grid item xs={12}>
-            <Box
-              sx={{
-                background: `${theme.colors.alpha.black[5]}`
-              }}
-            >
-              <Grid container spacing={0}>
-                <Grid item xs={12} md={6}>
-                  <Box
-                    p={4}
-                    sx={{
-                      background: `${theme.colors.alpha.white[70]}`
-                    }}
-                  >
-                    <Checklist />
-                  </Box>
-                </Grid>
-                <Grid item xs={12} md={6}>
-                  <Box p={4}>
-                    <Profile />
-                  </Box>
-                </Grid>
-              </Grid>
-            </Box>
-          </Grid>
-        </>
-      )}
-      {currentTab === 'taskSearch' && (
-        <Grid item xs={12}>
-          <Box p={4}>
-            <TaskSearch />
-          </Box>
-        </Grid>
-      )}
-      {currentTab === 'projectsBoard' && (
-        <Grid item xs={12}>
-          <Box
-            sx={{
-              py: { xs: 3, md: 8, lg: 12 },
-              textAlign: 'center'
+      <Grid item xs={12}>
+        <Box p={4}>
+          <Formik
+            initialValues={{
+              language: 'fr',
+              dateFormat: 'MM/DD/YY',
+              currency: 'MAD',
+              businessType: 'PAM',
+              autoAssignWorkOrder: true
             }}
+            validationSchema={Yup.object().shape({
+              language: Yup.string(),
+              dateFormat: Yup.string(),
+              currency: Yup.string(),
+              businessType: Yup.string(),
+              autoAssignWorkOrder: Yup.bool()
+            })}
+            onSubmit={onSubmit}
           >
-            <AvatarPrimary>
-              <NotificationsActiveTwoToneIcon />
-            </AvatarPrimary>
-            <Typography variant="h2">{t('No boards available')}</Typography>
-            <Typography
-              variant="h4"
-              sx={{
-                pt: 1,
-                pb: 3
-              }}
-              fontWeight="normal"
-              color="text.secondary"
-            >
-              {t(
-                'Browse the projects board application or create a new one right here'
-              )}
-              !
-            </Typography>
-            <Button
-              color="primary"
-              variant="outlined"
-              sx={{
-                borderWidth: '2px',
-                '&:hover': {
-                  borderWidth: '2px'
-                }
-              }}
-            >
-              {t('Create project board')}
-            </Button>
-          </Box>
-        </Grid>
-      )}
+            {({
+              errors,
+              handleBlur,
+              handleChange,
+              handleSubmit,
+              isSubmitting,
+              touched,
+              values
+            }) => (
+              <form onSubmit={handleSubmit}>
+                <Grid container spacing={3}>
+                  <Grid item xs={12}>
+                    <Grid container spacing={2}>
+                      <Grid item xs={12}>
+                        <Typography variant="h6" sx={{ mb: 0.5 }}>
+                          Language
+                        </Typography>
+                        <Field
+                          onChange={handleChange}
+                          value={values.language}
+                          as={Select}
+                          name="language"
+                        >
+                          <MenuItem value="en">English</MenuItem>
+                          <MenuItem value="fr">Français</MenuItem>
+                        </Field>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Typography variant="h6" sx={{ mb: 0.5 }}>
+                          Date format
+                        </Typography>
+                        <Field
+                          onChange={handleChange}
+                          value={values.dateFormat}
+                          as={Select}
+                          name="dateFormat"
+                        >
+                          <MenuItem value="MM/DD/YY">MM/DD/YY</MenuItem>
+                          <MenuItem value="DD/MM/YY">DD/MM/YY</MenuItem>
+                        </Field>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Typography variant="h6" sx={{ mb: 0.5 }}>
+                          Currency
+                        </Typography>
+                        <Field
+                          onChange={handleChange}
+                          value={values.currency}
+                          as={Select}
+                          name="currency"
+                        >
+                          <MenuItem value="MAD">MAD - Dirham - DH</MenuItem>
+                          <MenuItem value="EUR">EUR - Euro - €</MenuItem>
+                          <MenuItem value="USD">
+                            USD - United States Dollar - $
+                          </MenuItem>
+                        </Field>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Typography variant="h6" sx={{ mb: 0.5 }}>
+                          Business type
+                        </Typography>
+                        <Field
+                          onChange={handleChange}
+                          value={values.businessType}
+                          as={Select}
+                          name="businessType"
+                        >
+                          <MenuItem value="PAM">
+                            Physical asset management
+                          </MenuItem>
+                        </Field>
+                      </Grid>
+                    </Grid>
+                    <Divider sx={{ mt: 3 }} />
+                    <Grid container spacing={2} sx={{mt:1}}></Grid>
+                    <Grid item xs={12}>
+                      <Box
+                        display="flex"
+                        flexDirection="row"
+                        alignItems="center"
+                      >
+                        <Field
+                          onChange={handleChange}
+                          checked={values.autoAssignWorkOrder}
+                          as={Switch}
+                          name="autoAssignWorkOrder"
+                        />
+                        <Box display="flex" flexDirection="column">
+                          <Typography
+                            variant="h6"
+                            fontWeight="bold"
+                            sx={{ mb: 0.5 }}
+                          >
+                            Auto-assign Work Orders
+                          </Typography>
+                          <Typography variant="h6" fontStyle='italic'>
+                            Automatically assign new work orders to the person
+                            that creates them
+                          </Typography>
+                        </Box>
+                      </Box>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </form>
+            )}
+          </Formik>
+        </Box>
+      </Grid>
     </SettingsLayout>
   );
 }
