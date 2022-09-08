6632f0aa4ac60ae7c0ff05b82dcb539560e4a4c0
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2022-09-08 12:38:09 +0100

    feat: period selection

--- src/content/own/Subscription/Plans/index.tsx
+++ src/content/own/Subscription/Plans/index.tsx
@@ -3,9 +3,14 @@ import PersonTwoToneIcon from '@mui/icons-material/PersonTwoTone';
 import {
   Box,
   Card,
+  Divider,
+  FormControlLabel,
   Grid,
+  Radio,
+  RadioGroup,
   Slider,
   Stack,
+  styled,
   Typography,
   useTheme
 } from '@mui/material';
@@ -17,6 +22,11 @@ function Subscription() {
   const plan = { id: 'dsds4', name: 'Starter', users: 2 };
   const theme = useTheme();
   const [usersCount, setUsersCount] = useState<number>(3);
+  const [period, setPeriod] = useState<string>('monthly');
+  const periods = [
+    { name: 'Monthly', value: 'monthly' },
+    { name: 'Annually', value: 'annually' }
+  ];
 
   return (
     <>
@@ -55,49 +65,92 @@ function Subscription() {
           </Card>
           <Grid item xs={12}>
             <Card sx={{ p: 2, mt: 2 }}>
-              <Box
-                display="flex"
-                alignItems="center"
-                justifyContent="space-between"
-              >
-                <Box>
-                  <Typography variant="h4" gutterBottom>
-                    {t('Number of users who will use Grash')}
-                  </Typography>
-                  <Typography variant="subtitle2">
-                    {t('Pay only for')} <b>{t('Admin')}</b>,{' '}
-                    <b>{t('Technical')}</b> and <b>{t('Limited Technical')}</b>{' '}
-                    users, and use unlimited <b>{t('Requester')}</b>,{' '}
-                    <b>{t('View-Only')}</b> for free
-                  </Typography>
-                </Box>
-              </Box>
-              <Stack direction="row" spacing={2}>
-                <Slider
-                  sx={{ mt: 3 }}
-                  size="medium"
-                  value={usersCount}
-                  min={0}
-                  step={1}
-                  max={150}
-                  onChange={(_, value) => setUsersCount(value as number)}
-                />
-                <Typography
-                  sx={{
-                    p: 1,
-                    backgroundColor: theme.colors.alpha.black[5],
-                    border: 0.5,
-                    display: 'flex',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    borderRadius: 1
-                  }}
-                  fontWeight={'bold'}
-                  variant="h6"
+              <Box>
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="space-between"
                 >
-                  {usersCount}&nbsp;Users
+                  <Box>
+                    <Typography variant="h4" gutterBottom>
+                      {t('Number of users who will use Grash')}
+                    </Typography>
+                    <Typography variant="subtitle2">
+                      {t('Pay only for')} <b>{t('Admin')}</b>,{' '}
+                      <b>{t('Technical')}</b> and{' '}
+                      <b>{t('Limited Technical')}</b> users, and use unlimited{' '}
+                      <b>{t('Requester')}</b>, <b>{t('View-Only')}</b> for free
+                    </Typography>
+                  </Box>
+                </Box>
+                <Stack
+                  direction="row"
+                  spacing={2}
+                  sx={{ my: 3 }}
+                  alignItems={'center'}
+                >
+                  <Slider
+                    size="medium"
+                    value={usersCount}
+                    min={0}
+                    step={1}
+                    max={150}
+                    onChange={(_, value) => setUsersCount(value as number)}
+                  />
+                  <Typography
+                    sx={{
+                      p: 1,
+                      backgroundColor: theme.colors.alpha.black[5],
+                      border: 0.5,
+                      display: 'flex',
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                      borderRadius: 1
+                    }}
+                    fontWeight={'bold'}
+                    variant="h6"
+                  >
+                    {usersCount}&nbsp;Users
+                  </Typography>
+                </Stack>
+              </Box>
+              <Box>
+                <Typography variant="h4">
+                  {t('How would you like to be billed?')}
                 </Typography>
-              </Stack>
+                <RadioGroup
+                  sx={{ p: 2, my: 1 }}
+                  defaultValue={period}
+                  onChange={(event) => {
+                    setPeriod(event.target.value);
+                  }}
+                  name="period"
+                >
+                  <Stack direction="row" spacing={2}>
+                    {periods.map((item) => (
+                      <FormControlLabel
+                        key={item.value}
+                        sx={{
+                          border: 2,
+                          borderColor: theme.colors.primary.main,
+                          p: 2,
+                          backgroundColor:
+                            item.value === period
+                              ? theme.colors.primary.lighter
+                              : null
+                        }}
+                        value={item.value}
+                        control={<Radio />}
+                        label={
+                          <Typography variant="h6" fontWeight="bold">
+                            {item.name}
+                          </Typography>
+                        }
+                      />
+                    ))}
+                  </Stack>
+                </RadioGroup>
+              </Box>
             </Card>
           </Grid>
         </Grid>
