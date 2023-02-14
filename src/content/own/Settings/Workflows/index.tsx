55ce19025bf41a30708943bc64f7918241731aee
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2023-02-14 15:07:44 +0100

    chore: select conditions

--- src/content/own/Settings/Workflows/index.tsx
+++ src/content/own/Settings/Workflows/index.tsx
@@ -38,6 +38,21 @@ import {
   workOrderActions,
   workOrderConditions
 } from '../../../../models/owns/workflow';
+import { getVendorsMini } from '../../../../slices/vendor';
+import { getUsersMini } from '../../../../slices/user';
+import { getLocationsMini } from '../../../../slices/location';
+import { getCategories } from '../../../../slices/category';
+import { getAssetsMini } from '../../../../slices/asset';
+import { getTeamsMini } from '../../../../slices/team';
+import { getCurrencies } from '../../../../slices/currency';
+import { AssetMiniDTO } from '../../../../models/owns/asset';
+import { LocationMiniDTO } from '../../../../models/owns/location';
+import { PartMiniDTO } from '../../../../models/owns/part';
+import { getPartsMini } from '../../../../slices/part';
+import { UserMiniDTO } from '../../../../models/user';
+import { VendorMiniDTO } from '../../../../models/owns/vendor';
+import { TeamMiniDTO } from '../../../../models/owns/team';
+import Category from '../../../../models/owns/category';
 
 function Workflows() {
   const { t }: { t: any } = useTranslation();
@@ -47,11 +62,13 @@ function Workflows() {
   const [currentChecklist, setCurrentChecklist] = useState<Checklist>();
   const [currentMainCondition, setCurrentMainCondition] =
     useState<WFMainCondition>(mainConditions[0]);
+
   interface UICondition {
     conditionType: WorkflowConditionType;
     value: string | number;
     values?: string | number[];
   }
+
   const [currentConditions, setCurrentConditions] = useState<UICondition[]>([]);
   const [openDelete, setOpenDelete] = useState<boolean>(false);
   const { hasFeature } = useAuth();
@@ -60,21 +77,96 @@ function Workflows() {
   const { companySettingsId } = user;
   const { showSnackBar } = useContext(CustomSnackBarContext);
   const { workflows } = useSelector((state) => state.workflows);
+  const { vendorsMini } = useSelector((state) => state.vendors);
+  const { locationsMini, locationsHierarchy } = useSelector(
+    (state) => state.locations
+  );
+  const { categories } = useSelector((state) => state.categories);
+  const { usersMini } = useSelector((state) => state.users);
+  const { assetsMini } = useSelector((state) => state.assets);
+  const { teamsMini } = useSelector((state) => state.teams);
+  const { currencies } = useSelector((state) => state.currencies);
+  const { partsMini } = useSelector((state) => state.parts);
+
+  const fetchVendors = async () => {
+    if (!vendorsMini.length) dispatch(getVendorsMini());
+  };
+  const fetchParts = async () => {
+    if (!partsMini.length) dispatch(getPartsMini());
+  };
+  const fetchUsers = async () => {
+    if (!usersMini.length) dispatch(getUsersMini());
+  };
+  const fetchLocations = async () => {
+    if (!locationsMini.length) dispatch(getLocationsMini());
+  };
+  const fetchCategories = async (category: string) => {
+    if (!categories[category]) dispatch(getCategories(category));
+  };
+  const fetchAssets = async () => {
+    if (!assetsMini.length) dispatch(getAssetsMini());
+  };
+  const fetchTeams = async () => {
+    if (!teamsMini.length) dispatch(getTeamsMini());
+  };
+  const fetchCurrencies = async () => {
+    if (!currencies.length) dispatch(getCurrencies());
+  };
   type FieldType = 'text' | 'number' | 'select' | 'date' | 'dateRange';
-  interface ConditionField {
+
+  interface ConditionField<T> {
     type: FieldType;
-    items?: { label: string; value: number }[];
+    items?: T[];
     accessor: keyof WorkflowCondition;
     accessors?: (keyof WorkflowCondition)[];
-    formatter?: (value: string | number) => { id: number } | string | number;
+    formatter?: (value: T) => { label: string; value: string | number };
+    onOpen?: () => void;
   }
-  const conditionsConfig: Record<WorkflowConditionType, ConditionField> = {
-    ASSET_IS: { type: 'select', items: [], accessor: 'asset' },
+
+  const conditionsConfig: Record<
+    WorkflowConditionType,
+    ConditionField<
+      | AssetMiniDTO
+      | LocationMiniDTO
+      | PartMiniDTO
+      | string
+      | UserMiniDTO
+      | VendorMiniDTO
+      | TeamMiniDTO
+      | Category
+    >
+  > = {
+    ASSET_IS: {
+      type: 'select',
+      items: assetsMini,
+      accessor: 'asset',
+      onOpen: fetchAssets,
+      formatter: (asset: AssetMiniDTO) => ({
+        label: asset.name,
+        value: asset.id
+      })
+    },
     CATEGORY_IS: {
-      type: 'text',
+      type: 'select',
       accessor: currentMainCondition.startsWith('WORK_ORDER')
         ? 'workOrderCategory'
-        : 'purchaseOrderCategory'
+        : 'purchaseOrderCategory',
+      items:
+        categories[
+          currentMainCondition.startsWith('WORK_ORDER')
+            ? 'work-order-categories'
+            : 'purchase-order-categories'
+        ] ?? [],
+      onOpen: () =>
+        fetchCategories(
+          currentMainCondition.startsWith('WORK_ORDER')
+            ? 'work-order-categories'
+            : 'purchase-order-categories'
+        ),
+      formatter: (category: Category) => ({
+        label: category.name,
+        value: category.id
+      })
     },
     CREATED_AT_BETWEEN: {
       type: 'dateRange',
@@ -87,21 +179,70 @@ function Workflows() {
       accessor: null,
       accessors: ['startDate', 'endDate']
     },
-    LOCATION_IS: { type: 'select', items: [], accessor: 'location' },
+    LOCATION_IS: {
+      type: 'select',
+      items: locationsMini,
+      accessor: 'location',
+      onOpen: fetchLocations,
+      formatter: (location: LocationMiniDTO) => ({
+        label: location.name,
+        value: location.id
+      })
+    },
     NAME_CONTAINS: { type: 'text', accessor: 'label' },
     NAME_IS: { type: 'text', accessor: 'label' },
     NUMBER_VALUE_INFERIOR: { type: 'number', accessor: 'numberValue' },
     NUMBER_VALUE_SUPERIOR: { type: 'number', accessor: 'numberValue' },
-    PART_IS: { type: 'select', items: [], accessor: 'part' },
-    PRIORITY_IS: { type: 'select', items: [], accessor: 'priority' },
+    PART_IS: {
+      type: 'select',
+      items: partsMini,
+      accessor: 'part',
+      onOpen: fetchParts,
+      formatter: (part: PartMiniDTO) => ({ label: part.name, value: part.id })
+    },
+    PRIORITY_IS: {
+      type: 'select',
+      items: ['NONE', 'LOW', 'MEDIUM', 'HIGH'],
+      accessor: 'priority',
+      formatter: (priority: string) => ({ label: t(priority), value: priority })
+    },
     QUANTITY_INFERIOR: { type: 'number', accessor: 'numberValue' },
-    STATUS_IS: { type: 'select', items: [], accessor: 'priority' },
-    TEAM_IS: { type: 'select', items: [], accessor: 'team' },
+    STATUS_IS: {
+      type: 'select',
+      items: ['OPEN', 'IN_PROGRESS', 'ON_HOLD', 'COMPLETE'],
+      accessor: 'workOrderStatus',
+      formatter: (status: string) => ({ label: t(status), value: status })
+    },
+    TEAM_IS: {
+      type: 'select',
+      items: teamsMini,
+      accessor: 'team',
+      onOpen: fetchTeams,
+      formatter: (team: TeamMiniDTO) => ({ label: team.name, value: team.id })
+    },
     TITLE_CONTAINS: { type: 'text', accessor: 'value' },
-    USER_IS: { type: 'select', items: [], accessor: 'user' },
+    USER_IS: {
+      type: 'select',
+      items: usersMini,
+      accessor: 'user',
+      onOpen: fetchUsers,
+      formatter: (user: UserMiniDTO) => ({
+        label: `${user.firstName} ${user.lastName}`,
+        value: user.id
+      })
+    },
     VALUE_CONTAINS: { type: 'text', accessor: 'value' },
     VALUE_IS: { type: 'text', accessor: 'value' },
-    VENDOR_IS: { type: 'select', items: [], accessor: 'vendor' }
+    VENDOR_IS: {
+      type: 'select',
+      items: vendorsMini,
+      accessor: 'vendor',
+      onOpen: fetchVendors,
+      formatter: (vendor: VendorMiniDTO) => ({
+        label: vendor.companyName,
+        value: vendor.id
+      })
+    }
   };
   // @ts-ignore
   const mainConditionsConfig: Record<
@@ -212,7 +353,7 @@ function Workflows() {
           )}
         </Select>
         <Box sx={{ mt: 1 }}>
-          {['text', 'number'].includes(config.type) && (
+          {['text', 'number'].includes(config.type) ? (
             <TextField
               value={condition.value}
               onChange={(event) =>
@@ -220,7 +361,21 @@ function Workflows() {
               }
               type={config.type}
             />
-          )}
+          ) : config.type === 'select' ? (
+            <Select
+              value={condition.value}
+              onChange={(event) =>
+                handleConditionValueChange(event.target.value, index)
+              }
+              onOpen={config.onOpen}
+            >
+              {config.items.map((item, index) => (
+                <MenuItem key={index} value={config.formatter(item).value}>
+                  {config.formatter(item).label}
+                </MenuItem>
+              ))}
+            </Select>
+          ) : null}
         </Box>
       </Box>
     );
